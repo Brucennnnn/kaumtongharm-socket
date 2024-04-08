9e368478e03e22ac775cf7884b571d50981e001b
bruce <bruceoio@hotmail.com>
2024-04-08 18:03:44 +0700

    feat:update delete prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaService } from './prisma/prisma.service';
 import { ChatsModule } from './chats/chats.module';
 
 @Module({
   imports: [ChatsModule, ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
